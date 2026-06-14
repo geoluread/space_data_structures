85f1a7672f9659ac15c0dd660cbe897333aef423
Geoluread <43795552+geoluread@users.noreply.github.com>
2026-06-14 12:37:47 -0400

    Adding updates to linked list

--- src/main/java/SpaceLinkedList.java
+++ src/main/java/SpaceLinkedList.java
@@ -1,18 +1,26 @@
 import lombok.Getter;
 import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.PrintStream;
 import java.util.LinkedList;
 
 public class SpaceLinkedList {
 
-    Node head; //Head - null by default
+    static PrintStream p = System.out;
+    Node head;//Head - null by default
+    Node current;
     int counter;
+    Logger log = LoggerFactory.getLogger(SpaceDynamicArray.class);
 
     public SpaceLinkedList(){
+        log.info("Space Linked List Created");
         counter = 0;
     }
 
     public void add(String data){
+        log.info("Adding Node");
         Node newNode = new Node(data);
         counter++;
 
@@ -22,16 +30,90 @@ public class SpaceLinkedList {
         }
     }
 
-    public void traverseList(){
-
-    }
-
     public void insertInFront(String data){
 
+        log.info("Inserting Element to Front");
         Node old_head = head;
         head = new Node(data, old_head);
         counter++;
     }
 
+    public void insertToBack(String data){
+        log.info("Inserting Element to Back");
+        current = head;
+        if(current == null){
+            head = new Node(data);
+            counter++;
+        }
+        else{
+            while(current.has_next() == true){
+                current = current.getNext();
+            }
+            current.setNext(new Node(data));
+            counter++;
+        }
+    }
 
+    //Can return a null node
+    public String search(String target){
+        log.info("Searching Linked List");
+        current = head;
+        if(current == null){
+            p.println("List is empty. ["+target+"] does not exist");
+            log.info("List is empty. ["+target+"] does not exist");
+            return "Not Found";
+        }
+        else{
+            while(current != null){
+                if(current.getData() == target){
+                    log.info("Target Found. ["+target+"] does exist");
+                    return "Found";
+
+                }
+                current = current.getNext();
+            }
+            log.info("List is empty. ["+target+"] does not exist");
+            return "Not Found";
+        }
+
+    }
+
+    public void delete(String target){
+        log.info("Deleteing Element");
+        current = head;
+        Node previous = null;   //Used to track the previous node in the list
+
+        while(current != null){
+            if(current.getData() == target){
+                if(previous == null){
+                    head = current.getNext(); //This implies that the first node is the one being deleted
+                }
+                else {
+                    previous.setNext(current.getNext());
+                }
+                counter--;
+                log.info("Removing element ["+target+"]. Element has been found in the list");
+                return;
+            }
+            previous = current;
+            current = current.getNext();
+        }
+        log.error("No element with ["+target+"] found in the list");
+        return;
+    }
+
+    //Need to make it loop
+    public Node delete_from_front(String target){
+        current = head;
+        if(current != null){
+            if(current.getData() == target){
+                head = current.getNext();
+                counter--;
+                log.info("Removing element ["+target+"]. Element has been found in the list");
+                return head;
+            }
+        }
+        else return null;
+
+    }
 }
--- src/test/java/SpaceLinkedListTest.java
+++ src/test/java/SpaceLinkedListTest.java
@@ -2,26 +2,56 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 //import sun.jvm.hotspot.gc.shared.Space;
 
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SpaceLinkedListTest {
 
+    static PrintStream p = System.out;
+    //Grocery List Items
     SpaceLinkedList testOne;
 
     @BeforeEach
     void setUp(){
         testOne = new SpaceLinkedList();
-        testOne.add("chicken");
+//        testOne.add("chicken");
     }
 
     @Test
     void add() {
-        //Grocery List Items
+        //Adding an item to the list
         testOne.add("blueberries");
     }
 
     @Test
     void insertInFront(){
+        //Adding item to the top of the list - priority
         testOne.insertInFront("milk");
+        testOne.insertInFront("beef");
+        testOne.insertInFront("chicken");
+
+        p.println("Completed");
+    }
+
+    @Test
+    void insertToBack(){
+        //Adding item to the top of the list - priority
+//        testOne.insertInFront("milk");
+//        testOne.insertInFront("beef");
+        testOne.insertToBack("honey");
+
+        p.println("Completed");
+    }
+
+    @Test
+    void delete(){
+        testOne.insertInFront("A");
+        testOne.insertToBack("B");
+        testOne.insertToBack("C");
+
+        testOne.delete("A");
+
+        assertEquals("Not Found", testOne.search("A"));
     }
 }
